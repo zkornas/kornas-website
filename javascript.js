7e7fa3b198fb3b84078268ead30675184150c6a3
zkornas <zkornas@uw.edu>
2023-09-10 15:40:56 -0700

    Made stylistic changes, fixed sidebar button

--- javascript.js
+++ javascript.js
@@ -1,12 +1,23 @@
 var myPageIds = ['aboutme', 'myresearch', 'myprojects', 'contactme'];
+var isOpen = false;
+
+function sideBar() {
+    if (isOpen) {
+        closeSidebar();
+    } else {
+        openSidebar();
+    }
+}
 
 const openSidebar = () => {
+    isOpen = true;
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("sidebar").style.marginLeft = "250px";
     document.getElementById("blur").style.filter = 'blur(2px)';
 };
 
 const closeSidebar = () => {
+    isOpen = false;
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("sidebar").style.marginLeft = "0";
     document.getElementById("blur").style.filter = 'blur(0px)';
